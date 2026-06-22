97484ddb4a7f0b16097cb0b22d59e5a08414f8f5
Tihomir Kehayov <kehayov89@gmail.com>
2026-06-22 08:18:20 +0300

    init commit

--- fe/src/app/app.ts
+++ fe/src/app/app.ts
@@ -1,12 +1,47 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+export interface EmployeeCollaboration {
+  employeeIdOne: number;
+  employeeIdTwo: number;
+  projectId: number;
+  daysTogether: number;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, MatIconModule, MatDividerModule, MatButtonModule, MatGridListModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('fe');
+  protected selectedFileName = signal('');
+
+  protected responseData = signal<EmployeeCollaboration | null>(null);
+  private http = inject(HttpClient);
+
+  protected uploadFile(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const file = input?.files?.[0];
+    if (!file) return;
+
+    this.selectedFileName.set(file.name);
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    this.http.post<EmployeeCollaboration>('/employees/max-collaboration', formData).subscribe({
+      next: (response) => {
+        console.log('File uploaded successfully:', response);
+        this.responseData.set(response);
+      },
+      error: (error) => console.error('Upload failed:', error),
+    });
+  }
 }
